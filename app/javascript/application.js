08cfffd5fe82484ddc0d2bc5ca1b49a611e436ca
Farida-Hamid <faridaaddala@gmail.com>
2023-01-23 18:54:54 +0300

    Display React App from a deparate component

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-function App() {
-  return (<h1>Hello World!</h1>);
-}
+import App from './App';
 
 ReactDOM.render(
   <App/>,
